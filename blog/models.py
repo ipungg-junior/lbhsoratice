1d77fa25d2e6a5218eee4a9a7608e1900f43a384
ipungg-junior <ipungg.id@gmail.com>
2022-10-27 15:08:33 +0700

    Add summernote

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
-    img = models.ImageField()
     content = models.TextField()
+    created_on = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['-created_on']
 
     def __str__(self):
           return self.title
--- blog/urls.py
+++ blog/urls.py
@@ -7,3 +7,9 @@ urlpatterns = [
     path('blog/', Blog.as_view(context='blog-list')),
     path('blog/<slug:title>/', Blog.as_view(context='blog-detail')),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
